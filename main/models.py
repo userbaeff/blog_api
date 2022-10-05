3eadefdf1836ebc41597c676d09047bb5758502f
userbaeff <userbaeff@gmail.com>
2022-10-05 13:17:14 +0600

    blog_api

---
+++ main/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} --> {self.parent}' if self.parent else f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    body = models.TextField(blank=True)
+    owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')
+    preview = models.ImageField(upload_to='images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.owner} - {self.title}'
+
+    class Meta:
+        ordering = ('created_at',)
+        verbose_name = 'Публикация'
+        verbose_name_plural = 'Публикации'
---
+++ main/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Category, Post
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'owner', 'preview')
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('title', 'body', 'category', 'preview')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Post
+        fields = '__all__'
